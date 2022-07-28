5849f604245c6013897edac0a51b289191b38610
Suresh Thagunna <suresh.thagunna77@gmail.com>
2022-07-28 10:33:16 +0545

    setup database connection

--- coas_view/settings.py
+++ coas_view/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'coas_view.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'coasv',
+        'USER': 'hawk',
+        'PASSWORD': 'hawk',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
