0bce43db83ce6d3578484c665fca0f212e19c4bf
Suresh Thagunna <suresh.thagunna77@gmail.com>
2022-07-28 14:16:49 +0545

    general_view; handles POST request

--- data_entry/tests.py
+++ data_entry/tests.py
@@ -1,8 +1,25 @@
 from django.test import TestCase
 from django.urls import reverse
 
+from coasc.models import ImpersonalAccount, Split
+
 
 class GeneranJournalViewTest(TestCase):
     def test_uses_general_journal_template(self):
         response = self.client.get(reverse('data_entry:general_journal'))
+
         self.assertTemplateUsed(response, 'data_entry/general_journal.html')
+
+    def test_can_save_a_POST_request(self):
+        single_ac1 = ImpersonalAccount.objects.create(
+                name='single_ac1', type_ac='AS', code='1')
+        data = {'account': single_ac1.pk, 'type_split': 'dr', 'amount': 100}
+        url = reverse('data_entry:general_journal')
+        self.client.post(url, data=data)
+
+        saved_splits = Split.objects.all()
+
+        self.assertEqual(saved_splits.count(), 1)
+        self.assertEqual(saved_splits[0].account, single_ac1)
+        self.assertEqual(saved_splits[0].type_split, 'dr')
+        self.assertEqual(saved_splits[0].amount, 100)
--- data_entry/views.py
+++ data_entry/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 
+from coasc.models import Transaction
+
+from data_entry.forms import SplitForm
+
 
 def general_journal(request):
     template = 'data_entry/general_journal.html'
 
-    return render(request, template)
+    form = SplitForm(request.POST or None)
+    if form.is_valid():
+        transaction = Transaction.objects.create(description='description1')
+        form.instance.transaction = transaction
+        form.save()
+
+    context = {
+            'form': form,
+    }
+
+    return render(request, template, context)
