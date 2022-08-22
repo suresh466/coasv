cccde13955fb344121ca372807acabc8016875eb
Suresh Thagunna <suresh.thagunna77@gmail.com>
2022-08-22 18:21:20 +0545

    make single columned balance_sheet table

--- fstatements/views.py
+++ fstatements/views.py
@@ -38,32 +38,32 @@ def trial_balance(request):
 def balance_sheet(request):
     template = 'fs/balance_sheet.html'
 
-    as_acs = ImpersonalAccount.objects.filter(type_ac='AS')
     li_acs = ImpersonalAccount.objects.filter(type_ac='LI')
+    as_acs = ImpersonalAccount.objects.filter(type_ac='AS')
 
-    if not as_acs and not li_acs:
+    if not li_acs and not as_acs:
         return redirect(reverse('ledgers:general_ledger'))
 
-    as_acs = [{
-                'ac': ac, 'bal': ac.current_balance(),
-                'children': [{
-                    'ac': ca, 'bal': ca.current_balance()}
-                    for ca in ac.impersonalaccount_set.all()]
-    } for ac in as_acs]
     li_acs = [{
                 'ac': ac, 'bal': ac.current_balance(),
                 'children': [{
                     'ac': ca, 'bal': ca.current_balance()}
                     for ca in ac.impersonalaccount_set.all()]
     } for ac in li_acs]
+    as_acs = [{
+                'ac': ac, 'bal': ac.current_balance(),
+                'children': [{
+                    'ac': ca, 'bal': ca.current_balance()}
+                    for ca in ac.impersonalaccount_set.all()]
+    } for ac in as_acs]
 
-    as_total_bal = ImpersonalAccount.total_current_balance(type_ac='AS')
     li_total_bal = ImpersonalAccount.total_current_balance(type_ac='LI')
-    total_bal = {'as_bal': as_total_bal, 'li_bal': li_total_bal}
+    as_total_bal = ImpersonalAccount.total_current_balance(type_ac='AS')
     context = {
-            'as_acs': as_acs,
             'li_acs': li_acs,
-            'total_bal': total_bal,
+            'as_acs': as_acs,
+            'li_total_bal': li_total_bal,
+            'as_total_bal': as_total_bal,
     }
     return render(request, template, context)
 
