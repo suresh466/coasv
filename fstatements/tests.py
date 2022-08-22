91e5dcce3b33ccbaae50bb6d2487861a9b14d598
Suresh Thagunna <suresh.thagunna77@gmail.com>
2022-08-22 11:30:09 +0545

    create and pass balance_sheet table to templates

--- fstatements/tests.py
+++ fstatements/tests.py
@@ -1,8 +1,16 @@
 from django.test import TestCase
 from django.shortcuts import reverse
 
+from coasc.models import ImpersonalAccount
+
 
 class BalanceSheet(TestCase):
     def test_uses_balance_sheet_template(self):
+        ImpersonalAccount(name='single', code=1, type_ac='AS').save()
+
         response = self.client.get(reverse('fs:balance_sheet'))
         self.assertTemplateUsed(response, 'fs/balance_sheet.html')
+
+    def test_redirects_if_no_acs(self):
+        response = self.client.get(reverse('fs:balance_sheet'))
+        self.assertRedirects(response, reverse('ledgers:general_ledger'))
--- fstatements/views.py
+++ fstatements/views.py
@@ -1,6 +1,37 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect, reverse
+
+from coasc.models import ImpersonalAccount
 
 
 def balance_sheet(request):
     template = 'fs/balance_sheet.html'
-    return render(request, template)
+
+    as_acs = ImpersonalAccount.objects.filter(type_ac='AS')
+    li_acs = ImpersonalAccount.objects.filter(type_ac='LI')
+
+    if not as_acs and not li_acs:
+        # later do something that makes sense
+        return redirect(reverse('ledgers:general_ledger'))
+
+    as_acs = [{
+                'ac': ac, 'bal': ac.current_balance(),
+                'children': [{
+                    'ac': ca, 'bal': ca.current_balance()}
+                    for ca in ac.impersonalaccount_set.all()]
+    } for ac in as_acs]
+    li_acs = [{
+                'ac': ac, 'bal': ac.current_balance(),
+                'children': [{
+                    'ac': ca, 'bal': ca.current_balance()}
+                    for ca in ac.impersonalaccount_set.all()]
+    } for ac in li_acs]
+
+    as_total_bal = ImpersonalAccount.total_current_balance(type_ac='AS')
+    li_total_bal = ImpersonalAccount.total_current_balance(type_ac='LI')
+    total_bal = {'as_bal': as_total_bal, 'li_bal': li_total_bal}
+    context = {
+            'as_acs': as_acs,
+            'li_acs': li_acs,
+            'total_bal': total_bal,
+    }
+    return render(request, template, context)
