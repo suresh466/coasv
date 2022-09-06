e78a0e7fc502ff95c6bcc481ccd2d8edc3b7ea31
Suresh Thagunna <suresh.thagunna77@gmail.com>
2022-09-06 10:17:42 +0545

    create common templates base

--- coas_view/settings.py
+++ coas_view/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'coas_view.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'common/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- data_entry/admin.py
+++ data_entry/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from coasc.models import Ac
+from coasc.models import Ac, Member
 
 # Register your models here.
 
 admin.site.register(Ac)
+admin.site.register(Member)
