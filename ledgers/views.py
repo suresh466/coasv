0e64c5b7c5e14b1c586d375f09796ade69e532bf
Suresh thagunna <suresh.thagunna77@gmail.com>
2024-07-30 10:23:41 -0400

    migrate(ledgers/assets_ledger): to django-coasc v0.9.7.2

--- ledgers/views.py
+++ ledgers/views.py
@@ -167,29 +167,66 @@ def sales_ledger(request):
 def assets_ledger(request):
     template = "ledgers/assets_ledger.html"
 
-    acs = Ac.objects.filter(cat="AS")
-
-    headers = generate_simple_headers(acs)
-    footers = generate_simple_footers(acs)
-
-    txs = get_simple_txs(acs)
-    rows = generate_simple_rows(txs, acs)
-    bal_loaded_rows = load_rows_bal(rows)
-
-    grand_total = generate_grand_total(bal_loaded_rows)
-    total = Ac.total_bal(cat="AS")
-
-    loaded_rows = list(zip(txs, rows, grand_total))
-
-    table = {
-        "headers": headers,
-        "footers": footers,
-        "loaded_rows": loaded_rows,
-        "total": total,
+    # last row of transaction and footer balances are the same if everything is correct
+    # calling on get_flat_balances to get the balances is redundant and for tallying purposes
+    assets_account_balances = Ac.get_flat_balances("AS")
+    footer_grand_total = sum(
+        assets_account["balance"]["net_balance"]
+        for assets_account in assets_account_balances
+    )
+    running_balances = {
+        account_bals["account"].id: 0 for account_bals in assets_account_balances
     }
 
+    # fetch all assets accounts with prefetched splits for reduced database hits
+    splits = Split.objects.select_related("tx")
+    operating_accounts = Ac.objects.filter(
+        Q(cat="AS") | Q(p_ac__cat="AS")
+    ).prefetch_related(
+        Prefetch("split_set", queryset=splits, to_attr="prefetched_splits")
+    )
+    # get top level accounts for top level view of the ledger (no child acs)
+    top_level_accounts = [ac for ac in operating_accounts if not ac.is_child]
+
+    # list all the involved transactions
+    transactions = set()
+    for account in operating_accounts:
+        transactions.update(split.tx for split in account.prefetched_splits)
+
+    # prepare the rows for the ledger
+    rows = []
+    for transaction in transactions:
+        row = {"description": transaction.desc, "accounts_data": [], "grand_total": 0}
+        for account in top_level_accounts:
+            if account.is_parent:
+                child_accounts = [ac for ac in operating_accounts if ac.p_ac == account]
+                splits = []
+                for ca in child_accounts:
+                    child_splits = [
+                        s for s in ca.prefetched_splits if s.tx == transaction
+                    ]
+                    splits.extend(child_splits)
+            elif account.is_standalone:
+                splits = [s for s in account.prefetched_splits if s.tx == transaction]
+
+            debit = sum(split.am for split in splits if split.t_sp == "dr")
+            credit = sum(split.am for split in splits if split.t_sp == "cr")
+            net_balance = debit - credit
+            running_balances[account.id] += net_balance
+            row["accounts_data"].append(
+                {
+                    "debit": debit,
+                    "credit": credit,
+                    "running_balance": running_balances[account.id],
+                }
+            )
+            row["grand_total"] += running_balances[account.id]
+        rows.append(row)
+
     context = {
-        "table": table,
+        "ledger_data": assets_account_balances,
+        "rows": rows,
+        "footer_grand_total": footer_grand_total,
     }
 
     return render(request, template, context)
