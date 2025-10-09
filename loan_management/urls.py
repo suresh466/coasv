4a263a840377e1b7795660fc42008813cf1dcdbf
Suresh Thagunna <suresh.thagunna77@gmail.com>
2025-10-09 00:41:36 -0400

    feat: create unified payments page UI, setup regular interest payments

--- loan_management/urls.py
+++ loan_management/urls.py
@@ -7,6 +7,7 @@ app_name = "loan"
 urlpatterns = [
     path("", views.loans, name="loans"),
     path("<int:id>/", views.loan, name="loan"),
+    path("<int:id>/payment", views.payment, name="payment"),
     path("<int:id>/disburse", views.disburse, name="disburse"),
     path("<int:id>/pay-interest", views.pay_interest, name="pay_interest"),
 ]
--- loan_management/views.py
+++ loan_management/views.py
@@ -30,6 +30,35 @@ def loan(request, id):
     return render(request, template, context)
 
 
+def payment(request, id):
+    loan = Loan.objects.get(id=id)
+    template = "loan_management/payment.html"
+    payment_history = loan.generate_payment_history()
+    selected_payment = request.GET.get("payment_type")
+
+    if selected_payment is None or selected_payment == "interest":
+        total, period_start, period_end, days, leap_year = loan.calculate_interest()
+        calculated_interest = {
+            "total": int(total),
+            "period_start": period_start,
+            "period_end": period_end,
+            "days": days,
+            "leap_year": leap_year,
+        }
+    elif selected_payment == "principal":
+        pass
+    elif selected_payment == "to-date":
+        pass
+
+    context = {
+        "calculated_interest": locals().get("calculated_interest", None),
+        "loan": loan,
+        "payment_history": payment_history,
+    }
+
+    return render(request, template, context)
+
+
 @require_POST
 def disburse(request, id):
     loan = get_object_or_404(Loan, id=id)
