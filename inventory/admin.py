42f1aace6fe6b83c92dfffa849fb882f29b78967
Suresh Thagunna <suresh.thagunna77@gmail.com>
2025-10-31 16:44:09 -0400

    feat: exclude parent account from inventory account selection

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,6 +1,28 @@
+from coasc.models import Ac
 from django.contrib import admin
 
 from inventory.models import InventoryItem, Sale
 
-admin.site.register(InventoryItem)
-admin.site.register(Sale)
+
+class InventoryItemAdmin(admin.ModelAdmin):
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name in ["debit_account", "credit_account"]:
+            # Get all accounts and filter out parents
+            all_accounts = Ac.objects.all()
+            non_parent_ids = [ac.id for ac in all_accounts if not ac.is_parent]
+            kwargs["queryset"] = Ac.objects.filter(id__in=non_parent_ids)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+class SaleAdmin(admin.ModelAdmin):
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name in ["debit_account", "credit_account"]:
+            # Get all accounts and filter out parents
+            all_accounts = Ac.objects.all()
+            non_parent_ids = [ac.id for ac in all_accounts if not ac.is_parent]
+            kwargs["queryset"] = Ac.objects.filter(id__in=non_parent_ids)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+admin.site.register(InventoryItem, InventoryItemAdmin)
+admin.site.register(Sale, SaleAdmin)
